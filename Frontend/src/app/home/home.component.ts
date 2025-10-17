5132650f675b5477da5c2907341f1f9d8dd193fb
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-10-17 17:19:49 -0400

    dialog for create user

--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { UserTableComponent } from '../user-table/user-table.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
-  imports: [UserTableComponent, MatIcon],
+  imports: [
+    UserTableComponent, 
+    MatButtonModule, 
+    MatDialogModule,
+    MatIcon,
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  readonly dialog = inject(MatDialog);
 
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogCreateUser, {autoFocus: false});
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
+
+@Component({
+  selector: 'dialog-create-user',
+  templateUrl: 'dialog-create-user.html',
+  imports: [MatDialogModule, MatButtonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DialogCreateUser {}
