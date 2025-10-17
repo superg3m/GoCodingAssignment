0f4a144c8baae9c81ae5820ab2d8eeb590167af5
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-10-17 16:39:20 -0400

    home component and table

--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { UserTableComponent } from '../user-table/user-table.component';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [UserTableComponent, MatIcon],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
