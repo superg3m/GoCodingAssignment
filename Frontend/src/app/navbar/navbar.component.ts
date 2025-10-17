1c8c0f80ec2dd9ebb6f82c72562efba32834b936
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-10-17 15:42:16 -0400

    navbar component

---
+++ Frontend/src/app/navbar/navbar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  imports: [MatIconModule, RouterLink],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+
+export class NavbarComponent {
+
+}
