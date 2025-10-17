3f4e6990d98ce24ef54c3735e1b2d3152d140b2a
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-10-17 18:52:15 -0400

    dialog

--- Frontend/src/app/home/home.component.ts
+++ Frontend/src/app/home/home.component.ts
@@ -3,6 +3,10 @@ import { UserTableComponent } from '../user-table/user-table.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatIcon } from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-home',
@@ -30,7 +34,15 @@ export class HomeComponent {
 @Component({
   selector: 'dialog-create-user',
   templateUrl: 'dialog-create-user.html',
-  imports: [MatDialogModule, MatButtonModule],
+  imports: [
+    MatDialogModule, 
+    MatButtonModule, 
+    MatInputModule, 
+    MatFormFieldModule, 
+    FormsModule,
+    MatSelectModule,
+    MatIcon
+  ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DialogCreateUser {}
