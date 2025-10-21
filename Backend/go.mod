3daf8c8f17829cb9c726f923274bb8186de02087
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-10-21 14:25:36 -0400

    swagger is working. Its actually pretty cool

--- Backend/go.mod
+++ Backend/go.mod
@@ -10,28 +10,44 @@ require (
 	github.com/lib/pq v1.10.9
 	github.com/onsi/ginkgo/v2 v2.26.0
 	github.com/onsi/gomega v1.38.2
+	github.com/swaggo/echo-swagger v1.4.1
+	github.com/swaggo/swag v1.16.6
 )
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/Masterminds/semver/v3 v3.4.0 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-openapi/jsonpointer v0.22.1 // indirect
+	github.com/go-openapi/jsonreference v0.21.2 // indirect
+	github.com/go-openapi/spec v0.22.0 // indirect
+	github.com/go-openapi/swag/conv v0.25.1 // indirect
+	github.com/go-openapi/swag/jsonname v0.25.1 // indirect
+	github.com/go-openapi/swag/jsonutils v0.25.1 // indirect
+	github.com/go-openapi/swag/loading v0.25.1 // indirect
+	github.com/go-openapi/swag/stringutils v0.25.1 // indirect
+	github.com/go-openapi/swag/typeutils v0.25.1 // indirect
+	github.com/go-openapi/swag/yamlutils v0.25.1 // indirect
 	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
 	github.com/google/go-cmp v0.7.0 // indirect
 	github.com/google/pprof v0.0.0-20250403155104-27863c87afa6 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/swaggo/files/v2 v2.0.2 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	go.uber.org/automaxprocs v1.6.0 // indirect
 	go.yaml.in/yaml/v3 v3.0.4 // indirect
-	golang.org/x/crypto v0.41.0 // indirect
-	golang.org/x/mod v0.27.0 // indirect
-	golang.org/x/net v0.43.0 // indirect
-	golang.org/x/sync v0.16.0 // indirect
-	golang.org/x/sys v0.35.0 // indirect
-	golang.org/x/text v0.28.0 // indirect
+	golang.org/x/crypto v0.43.0 // indirect
+	golang.org/x/mod v0.29.0 // indirect
+	golang.org/x/net v0.46.0 // indirect
+	golang.org/x/sync v0.17.0 // indirect
+	golang.org/x/sys v0.37.0 // indirect
+	golang.org/x/text v0.30.0 // indirect
 	golang.org/x/time v0.11.0 // indirect
-	golang.org/x/tools v0.36.0 // indirect
+	golang.org/x/tools v0.38.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- Backend/main.go
+++ Backend/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/superg3m/server/Model"
+	echoSwagger "github.com/swaggo/echo-swagger"
 	"net/http"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -11,7 +12,13 @@ import (
 	"github.com/labstack/echo/v4"
 	_ "github.com/lib/pq"
 	"github.com/superg3m/server/Utility"
+	_ "github.com/superg3m/server/docs"
 )
+
+// Note(Jovanni):
+// Probably you could use dependency injection to not have this global
+// but I think its fine for now.
+var db *sqlx.DB
 
 // Note(Jovanni):
 // If I was doing something more serious you would use some type of
@@ -25,16 +32,21 @@ const (
 	DBNAME    = "stoic"
 )
 
+// @title Stoic API
+// @version 1.0
+// @description User management API for Stoic.
+// @host localhost:8080
+// @BasePath /
 func main() {
-	// Connect to DB
 	dsn := Utility.GetDSN(DB_ENGINE, HOST, PORT, USER, PASSWORD, DBNAME)
-	db, err := sqlx.Connect(DB_ENGINE, dsn)
-	if err != nil {
+	var err error
+	if db, err = sqlx.Connect(DB_ENGINE, dsn); err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
 	e := echo.New()
+	e.GET("/swagger/*", echoSwagger.WrapHandler)
 
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins:     []string{"http://localhost:4200"},
@@ -42,7 +54,6 @@ func main() {
 		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept},
 		AllowCredentials: true,
 	}))
-
 	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			c.Response().Header().Set("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
@@ -50,141 +61,142 @@ func main() {
 		}
 	})
 
-	e.POST("/User/Create", func(c echo.Context) error {
-		var u Model.User
-		err := c.Bind(&u)
-		if err != nil {
-			return c.JSON(http.StatusBadRequest, err.Error())
-		}
+	e.POST("/User/Create", CreateUser)
+	e.GET("/User/Get/All", GetAllUsers)
+	e.GET("/User/Get/:id", GetUserByID)
+	e.PATCH("/User/Update", UpdateUser)
+	e.DELETE("/User/Delete", DeleteUser)
 
-		_, err = db.Exec("SELECT * FROM User WHERE user_name = ?", u.Username)
-		if err != nil {
-			return c.JSON(http.StatusConflict, "Found duplicate Username")
-		}
-
-		if !Utility.ValidEmail(u.Email) {
-			return c.JSON(http.StatusBadRequest, "Invalid Email")
-		}
-
-		sql := `INSERT INTO User (
-        	user_name, 
-        	first_name,
-        	last_name,
-            email,
-            user_status,
-            department
-        ) VALUES (?, ?, ?, ?, ?, ?)
-		`
-
-		var dbUser Model.User
-		err = db.Get(&dbUser, "SELECT * FROM User WHERE user_name = ?", u.Username)
-		if err == nil {
-			return c.JSON(http.StatusConflict, "Username already exists")
-		}
-
-		result, err := db.Exec(sql, u.Username, u.Firstname, u.Lastname, u.Email, u.UserStatus, u.Department)
-		if err != nil {
-			return c.JSON(http.StatusBadRequest, err.Error())
-		}
-
-		u.ID, _ = result.LastInsertId()
-		return c.JSON(http.StatusOK, u)
-	})
-
-	e.GET("/User/Get/All", func(c echo.Context) error {
-		var users []Model.User
-		err := db.Select(&users, "SELECT * FROM User")
-		if err != nil {
-			return c.JSON(http.StatusNotFound, err.Error())
-		}
-
-		return c.JSON(http.StatusOK, users)
-	})
-
-	// NOTE(Jovanni):
-	// This error can't really happen in a small scoped thing like this
-	// However if there was like multiple people reading and writing to the
-	// database then you could probably have a situation.
-	e.GET("/User/Get/:id", func(c echo.Context) error {
-		id := c.Param("id")
-
-		var u Model.User
-		err := db.Get(&u, "SELECT * FROM User WHERE user_id = ?", id)
-		if u.ID != 0 && err != nil {
-			return c.JSON(http.StatusNotFound, err.Error())
-		}
-
-		return c.JSON(http.StatusOK, u)
-	})
-
-	e.PATCH("/User/Update", func(c echo.Context) error {
-		var u Model.User
-		err := c.Bind(&u)
-		if u.ID != 0 && err != nil {
-			return c.JSON(http.StatusBadRequest, err.Error())
-		}
-
-		exists := false
-		err = db.Get(&exists, "SELECT EXISTS(SELECT 1 FROM User WHERE user_id = ?)", u.ID)
-		if err != nil {
-			return c.JSON(http.StatusBadRequest, err.Error())
-		}
-		if !exists {
-			return c.JSON(http.StatusBadRequest, "User ID doesn't exist exists")
-		}
-
-		if !Utility.ValidEmail(u.Email) {
-			return c.JSON(http.StatusBadRequest, "Invalid Email")
-		}
-
-		var dbUser Model.User
-		err = db.Get(&dbUser, "SELECT * FROM User WHERE user_name = ?", u.Username)
-		if err == nil && dbUser.ID != u.ID {
-			return c.JSON(http.StatusConflict, "Username already exists")
-		}
-
-		sql := `UPDATE User
-		SET user_name = ?, 
-		first_name = ?,
-		last_name = ?,
-		email = ?,
-		user_status = ?,
-		department = ?
-		WHERE user_id = ?
-		`
-
-		_, err = db.Exec(sql, u.Username, u.Firstname, u.Lastname, u.Email, u.UserStatus, u.Department, u.ID)
-		if err != nil {
-			return c.JSON(http.StatusNotFound, err.Error())
-		}
-
-		return c.JSON(http.StatusOK, "User Updated Successfully")
-	})
-
-	// Make sure to check to make sure user_name is unique
-	e.DELETE("/User/Delete", func(c echo.Context) error {
-		var u Model.User
-		err := c.Bind(&u)
-		if err != nil {
-			return c.JSON(http.StatusBadRequest, err.Error())
-		}
-
-		sql := "DELETE FROM User WHERE user_id = ?"
-
-		result, err := db.Exec(sql, u.ID)
-		rows, _ := result.RowsAffected()
-		if rows == 0 {
-			return c.JSON(http.StatusBadRequest, "User not found")
-		}
-		if err != nil {
-			return c.JSON(http.StatusInternalServerError, err.Error())
-		}
-
-		return c.JSON(http.StatusOK, "User Deleted Successfully")
-	})
-
-	err = e.Start(":8080")
-	if err != nil {
+	if err := e.Start(":8080"); err != nil {
 		fmt.Println(err)
 	}
+}
+
+// @Summary Create user
+// @Description Creates a new user
+// @Tags User
+// @Accept json
+// @Produce json
+// @Param user body Model.User true "User data"
+// @Success 200 {object} Model.User
+// @Failure 400 {string} string
+// @Failure 409 {string} string
+// @Router /User/Create [post]
+func CreateUser(c echo.Context) error {
+	var u Model.User
+	if err := c.Bind(&u); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	if !Utility.ValidEmail(u.Email) {
+		return c.JSON(http.StatusBadRequest, "Invalid Email")
+	}
+
+	var dbUser Model.User
+	if err := db.Get(&dbUser, "SELECT * FROM User WHERE user_name = ?", u.Username); err == nil {
+		return c.JSON(http.StatusConflict, "Username already exists")
+	}
+
+	sqlStmt := `INSERT INTO User (user_name, first_name, last_name, email, user_status, department)
+	            VALUES (?, ?, ?, ?, ?, ?)`
+	result, err := db.Exec(sqlStmt, u.Username, u.Firstname, u.Lastname, u.Email, u.UserStatus, u.Department)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	u.ID, _ = result.LastInsertId()
+	return c.JSON(http.StatusOK, u)
+}
+
+// @Summary Get all users
+// @Tags User
+// @Produce json
+// @Success 200 {array} Model.User
+// @Failure 404 {string} string
+// @Router /User/Get/All [get]
+func GetAllUsers(c echo.Context) error {
+	var users []Model.User
+	if err := db.Select(&users, "SELECT * FROM User"); err != nil {
+		return c.JSON(http.StatusNotFound, err.Error())
+	}
+	return c.JSON(http.StatusOK, users)
+}
+
+// @Summary Get user by ID
+// @Tags User
+// @Produce json
+// @Param id path int true "User ID"
+// @Success 200 {object} Model.User
+// @Failure 404 {string} string
+// @Router /User/Get/{id} [get]
+func GetUserByID(c echo.Context) error {
+	id := c.Param("id")
+	var u Model.User
+	if err := db.Get(&u, "SELECT * FROM User WHERE user_id = ?", id); err != nil {
+		return c.JSON(http.StatusNotFound, err.Error())
+	}
+	return c.JSON(http.StatusOK, u)
+}
+
+// @Summary Update user
+// @Tags User
+// @Accept json
+// @Produce json
+// @Param user body Model.User true "Updated user data"
+// @Success 200 {string} string
+// @Failure 400 {string} string
+// @Failure 404 {string} string
+// @Router /User/Update [patch]
+func UpdateUser(c echo.Context) error {
+	var u Model.User
+	if err := c.Bind(&u); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	if !Utility.ValidEmail(u.Email) {
+		return c.JSON(http.StatusBadRequest, "Invalid Email")
+	}
+
+	var exists bool
+	if err := db.Get(&exists, "SELECT EXISTS(SELECT 1 FROM User WHERE user_id = ?)", u.ID); err != nil || !exists {
+		return c.JSON(http.StatusBadRequest, "User ID doesn't exist")
+	}
+
+	var dbUser Model.User
+	if err := db.Get(&dbUser, "SELECT * FROM User WHERE user_name = ?", u.Username); err == nil && dbUser.ID != u.ID {
+		return c.JSON(http.StatusConflict, "Username already exists")
+	}
+
+	sqlStmt := `UPDATE User SET user_name=?, first_name=?, last_name=?, email=?, user_status=?, department=? WHERE user_id=?`
+	if _, err := db.Exec(sqlStmt, u.Username, u.Firstname, u.Lastname, u.Email, u.UserStatus, u.Department, u.ID); err != nil {
+		return c.JSON(http.StatusNotFound, err.Error())
+	}
+	return c.JSON(http.StatusOK, "User Updated Successfully")
+}
+
+// @Summary Delete user
+// @Tags User
+// @Accept json
+// @Produce json
+// @Param user body Model.User true "User data"
+// @Success 200 {string} string
+// @Failure 400 {string} string
+// @Failure 500 {string} string
+// @Router /User/Delete [delete]
+func DeleteUser(c echo.Context) error {
+	var u Model.User
+	if err := c.Bind(&u); err != nil {
+		return c.JSON(http.StatusBadRequest, err.Error())
+	}
+
+	result, err := db.Exec("DELETE FROM User WHERE user_id = ?", u.ID)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, err.Error())
+	}
+	rows, _ := result.RowsAffected()
+	if rows == 0 {
+		return c.JSON(http.StatusBadRequest, "User not found")
+	}
+
+	return c.JSON(http.StatusOK, "User Deleted Successfully")
 }
