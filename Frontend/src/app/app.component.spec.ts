27162fe94a0841a4837c3bd334c08bce2fb07ef9
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-10-21 08:18:42 -0400

    ok wrote some angular tests!

--- Frontend/src/app/app.component.spec.ts
+++ Frontend/src/app/app.component.spec.ts
@@ -1,29 +1,25 @@
-import { TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideRouter} from '@angular/router';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
+      providers: [provideRouter([])]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
   it(`should have the 'Frontend' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Frontend');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Frontend');
+    expect(component.title).toEqual('Frontend');
   });
 });
--- Frontend/src/app/home/home.component.spec.ts
+++ Frontend/src/app/home/home.component.spec.ts
@@ -1,23 +1,58 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { HomeComponent } from './home.component';
+import {Department, HomeComponent, UserStatus} from './home.component';
+import {By} from '@angular/platform-browser';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
-
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HomeComponent]
-    })
-    .compileComponents();
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
-    component = fixture.componentInstance;
+    fixture.componentInstance.users = [
+      {
+        user_id: 1,
+        user_name: 'bob',
+        first_name: 'Bob',
+        last_name: 'Smith',
+        email: 'bob@test.com',
+        user_status: UserStatus.ACTIVE,
+        department: Department.ENGINEER
+      }
+    ];
+
     fixture.detectChanges();
+    component = fixture.componentInstance;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should open create dialog', () => {
+    spyOn(component, "openCreateDialog")
+    const editButton = fixture.debugElement.query(By.css('.create-btn'));
+    editButton.triggerEventHandler('click', null);
+
+    expect(component.openCreateDialog).toHaveBeenCalled();
+  })
+
+  it('should open edit dialog', () => {
+    spyOn(component, "openEditDialog")
+    const editButton = fixture.debugElement.query(By.css('.edit-btn'));
+    editButton.triggerEventHandler('click', null);
+
+    expect(component.openEditDialog).toHaveBeenCalled();
+  })
+
+  it('should open delete dialog', () => {
+    spyOn(component, "openDeleteDialog")
+    const deleteButton = fixture.debugElement.query(By.css('.delete-btn'));
+    deleteButton.triggerEventHandler('click', null);
+
+    expect(component.openDeleteDialog).toHaveBeenCalled();
+  })
 });
